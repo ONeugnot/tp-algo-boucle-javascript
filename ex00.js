c6923dcfea3ab7fe55684c106e2e33cf2843d4f6
Olivier NEUGNOT <oliviergenilac@gmail.com>
2024-11-22 14:06:58 +0100

    feat(add): ex00

---
+++ ex00.js
@@ -0,0 +1,10 @@
+function sumArray(arr) {
+	let result = 0;
+    for (let i = 0 ; i < arr.length; i++) {
+        result = result + arr[i]
+    }
+  return result
+}
+
+console.log(sumArray([1, 2, 3, 4])); // Résultat attendu : 10
+console.log(sumArray([10, -2, 5])); // Résultat attendu : 13
