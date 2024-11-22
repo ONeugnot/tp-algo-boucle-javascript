61f6f81e3690a38c6074d4e8b9c4d0de9c5f02dc
Olivier NEUGNOT <oliviergenilac@gmail.com>
2024-11-22 14:54:17 +0100

    add ex01

--- ex00.js
+++ ex00.js
@@ -7,4 +7,4 @@ function sumArray(arr) {
 }
 
 console.log(sumArray([1, 2, 3, 4])); // Résultat attendu : 10
-console.log(sumArray([10, -2, 5])); // Résultat attendu : 13
+console.log(sumArray([10, -2, 5])); // Résultat attendu : 13a
---
+++ ex01.js
@@ -0,0 +1,7 @@
+function sumArray(arr) {
+	// Écrire le code ici
+}
+
+// Exemples de test :
+console.log(sumArray([1, 2, 3, 4])); // Résultat attendu : 10
+console.log(sumArray([10, -2, 5])); // Résultat attendu : 13
